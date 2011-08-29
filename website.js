ed7d0ba86b4429ea7dd89b192c9ea189fa81fd97
Richard Anaya <richard.anaya@gmail.com>
2011-08-29 05:31:03 -0500

    first commit

---
+++ website.js
@@ -0,0 +1,26 @@
+var express = require('express');
+
+var server = express.createServer();
+
+server.configure( 
+    function() {
+	server.use(express.static(__dirname + '/root/'));
+	server.enable("jsonp callback");
+    }
+);
+
+server.get('/motd', 
+    function(req, res) {
+	res.json('All your base are belong to us '+req.query.name+'.');
+    }
+);
+
+server.get(/^.*$/, 
+    function(req, res) {
+	res.redirect('/index.html');
+    }
+);
+
+var port = process.env.PORT || 9999;
+
+server.listen(port);
